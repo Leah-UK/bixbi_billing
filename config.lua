c114d5396aff448c7eab8f27cdde29f924af98fb
Leah <mail@leahuk.com>
2022-01-18 09:01:33 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,8 +2,8 @@ Config = {}
 
 Config.Debug = false
 Config.Time =  60 * 60000 -- How long after getting a Fine/Bill you automatically pay it.
-Config.AllowedJobs = {
+Config.AllowedJobs = { -- Add jobs that are able to give bills.
     police = { maxBill = 10000 },
     ambulance = { maxBill = 10000 },
     mechanic = { maxBill = 10000 }
-}
+} -- To give them qtarget capabilities you must edit SetupTargets() in client.lua.
